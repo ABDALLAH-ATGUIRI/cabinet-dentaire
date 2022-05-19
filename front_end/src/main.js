bb3131b9b0cd70560630c9939188e379eb64742f
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2022-05-19 09:51:25 +0100

    last commit

--- front_end/src/main.js
+++ front_end/src/main.js
@@ -1,10 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './scss/main.scss'
+import { createApp } from "vue";
+import { createStore } from "vuex";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./scss/main.scss";
 
+// createStore(App).use(vuex);
+createApp(App).use(store).use(router).mount("#app");
 
-createApp(App).use(store).use(router).mount('#app')
-
-
+new Vue({
+  store: store,
+  render: (h) => h(App),
+}).$mount("#app");
--- front_end/src/store/index.js
+++ front_end/src/store/index.js
@@ -1,14 +1,39 @@
-import { createStore } from 'vuex'
+// import {createApp} from 'vue'
+// import {createStore} from 'vuex'
 
-export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+// const store = createStore({
+//     state()
+//     {
+//       return {
+//         count: 0
+//       }
+//     },
+//     mutations: {
+//       increment (state)
+//       {
+//         state.count++
+//       }
+//     }
+// })
+
+
+// const app = createApp({
+
+// })
+
+
+// app.use(store)
+  
+
+// export default VueX({
+//   state: {
+//   },
+//   getters: {
+//   },
+//   mutations: {
+//   },
+//   actions: {
+//   },
+//   modules: {
+//   }
+// })
